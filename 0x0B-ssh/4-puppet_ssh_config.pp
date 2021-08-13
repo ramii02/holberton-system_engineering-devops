d93728f26c8e6f4660c2efaacfe223a6384cbc94
ramii02 <louhibirami11@gmail.com>
2021-08-13 03:02:07 +0200

    devops

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+# Make changes to configuration file
+
+file_line {'unable password authentication':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
+}
+
+file_line {'add identity file':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/holberton',
+}
